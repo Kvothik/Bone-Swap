5979248cc4923829e00f79d1a81f64deca6b6bf6
kvothik <cromeans15@gmail.com>
2022-06-19 09:28:03 -0500

    working on user unit test. removed strict mode causing double render

--- server/tests/users.test.js
+++ server/tests/users.test.js
@@ -1,4 +1,6 @@
 import axios from "axios";
+import request from "supertest";
+import { createUser } from '../controllers/usersController';
 jest.mock("axios");
 const User = require('../models/Users');
 
@@ -7,23 +9,37 @@ beforeAll(async () => await db.connect());
 afterEach(async () => await db.clearDatabase());
 afterAll(async () => await db.closeDatabase());
 
+const request = testClient(createUser);
+
 describe('CREATE & GET by ID users tests', () => {
     it('Create User Test', async done => {
-        const testUser = new User({
-            // _id: 999,
+        const payload = new User({
+            _id: 999,
             Username: "UnitTestUser",
             ProfilePicture: "thisIsAURLString",
             Password: "Password",
             SecurityEnablement: false
         });
-        // Create user
-        axios.post(`http://localhost:8080/users/createUser`, testUser).then(res => {
-            axios.get('http://localhost:8080/users/getUserByID', res.body._id).then((testUserDB) => {
-                console.log(testUserDB);
-                // expect(testUserDB._id).toBe(999);
-                done()
-            });
+
+        const res = await request
+            .post("http://localhost:8080/users/createUser")
+            .set({
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            })
+            .send(JSON.stringify(payload));
+        expect(res.status).toBe(200);
+        expect(res.body.Username).toEqual({
+            name: 'UnitTestUser',
         });
+        // Create user
+        // axios.post(`http://localhost:8080/users/createUser`, testUser).then(res => {
+        //     axios.get('http://localhost:8080/users/getUserByID', res.body._id).then((testUserDB) => {
+        //         console.log(testUserDB);
+        //         expect(testUserDB._id).toBe(999);
+        //         done()
+        //     });
+        // });
         // createUser(testUser).then((data) => {
         //     // Get user that was created
         //     getUserByID(testUser._id).then((testUserDB) => {
